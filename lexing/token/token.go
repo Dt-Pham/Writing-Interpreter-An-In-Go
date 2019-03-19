aae24a42185a4d5d93e8ccd0e21765a757650938
Dt Pham <dt99.pham@gmail.com>
2019-03-18 22:38:53 -0700

    Add constants to package token.

--- lexing/token/token.go
+++ lexing/token/token.go
@@ -3,6 +3,35 @@ package token
 // TokenType represent the type of token in Monkey Programming Language
 type TokenType string
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literal
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+
+	COMMAS    = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "{"
+
+	// Keywords
+
+	LET      = "LET"
+	FUNCTION = "FUNCTION"
+)
+
 // Token struct int Monkey Programming Language
 type Token struct {
 	Type    TokenType
