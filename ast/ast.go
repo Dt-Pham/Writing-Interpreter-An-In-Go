f6b679d1d989e3c714778ee0fa534b1d6eea894b
Dt Pham <dt99.pham@gmail.com>
2019-03-23 11:20:54 -0700

    Extend the ast package.

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,14 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 // Node ...
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement ...
@@ -32,6 +36,15 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 // LetStatement ...
 type LetStatement struct {
 	Token token.Token // the LET token
@@ -44,6 +57,20 @@ func (ls *LetStatement) statementNode() {}
 // TokenLiteral ...
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.TokenLiteral())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
 }
 
 // ReturnStatement ...
@@ -59,6 +86,38 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// ExpressionStatement ...
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral ...
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
 // Identifier ...
 type Identifier struct {
 	Token token.Token // the IDENT token
@@ -70,4 +129,8 @@ func (i *Identifier) expressionNode() {}
 // TokenLiteral ...
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
