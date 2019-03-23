1f1747cf509237901351da518bb276da5cc5c526
Dt Pham <dt99.pham@gmail.com>
2019-03-23 12:53:01 -0700

    Write test for IntegerLiteralExpression.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -104,6 +104,38 @@ func TestIdentifierExpression(t *testing.T) {
 	}
 }
 
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseError(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 element. Got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. Got=%T", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("expression is not *ast.Identifer. Got=%T", stmt.Expression)
+	}
+
+	if literal.Value != 5 {
+		t.Errorf("ident.Value is not %d. got=%d", 5, literal.Value)
+	}
+
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("ident.TokenLiteral is not %s. got=%s", "5", literal.TokenLiteral())
+	}
+}
+
 func checkParseError(t *testing.T, p *Parser) {
 	if len(p.errors) == 0 {
 		return
